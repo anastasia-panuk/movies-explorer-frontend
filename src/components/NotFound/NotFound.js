d8d088c9e32e87a5e15db4fb72e76b847b574fe5
Anastasia Panuk <AnaPanana@yandex.ru>
2023-05-24 23:35:38 +0300

    fix

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -9,7 +9,7 @@ function NotFound() {
         <h2 className="not-found__code">404</h2>
         <p className="not-found__text">Страница не найдена</p>
       </div>
-      <Link onClick={() => navigate(-2)} className="not-found__link">
+      <Link onClick={() => navigate(-3)} className="not-found__link">
         Назад
       </Link>
     </main>
